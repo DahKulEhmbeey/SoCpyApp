ada091f2ffd65a8a441b3db4da1868ac771ed5b1
Mubarak Bello <bellomuba.rak0@gmail.com>
2019-05-02 10:19:29 +0100

    Added code skeleton for saving a contact. TODO: Edit the `add_contact` function

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 import json
 from flask import Flask, render_template, request
-from src.edit import edit_contact
-from src.save import add_contact
-from src.search import search_for_contact
-
 
 app = Flask(__name__)
+
+CONTACTS_LIST = []
+
+
+def add_contact(name, number):
+  global CONTACTS_LIST
+  # 1. Add a new contact to the CONTACTS_LIST.
+  # 2. Deduce a way to check if the operation was successful
+  #    and return either True or False indicating the operation status.
+  #    Return True if there is an error, and False if there is no error.
+  error_status = False # False by default, return the appropriate boolean
+  return error_status
+
+
+def edit_contact():
+  global CONTACTS_LIST
+  # Edit a particular contact and return either True
+  # or False indication the operation status
+  status = False # False by default, return the appropriate boolean
+  return status
+
+
+def search_for_contact():
+  global CONTACTS_LIST
+  # Search for a particular contact and return either True
+  # or False indication the operation status
+  status = False # False by default, return the appropriate boolean
+  return status
+
 
 @app.route('/')
 def index():
@@ -14,16 +39,17 @@ def index():
 
 @app.route('/contacts')
 def contacts():
-  return 'Contacts here'
+  return json.dumps(CONTACTS_LIST)
 
 
 @app.route('/add-contact', methods=['POST'])
 def save_contact():
   name = request.json['name']
   number = request.json['number']
-  op_status = add_contact(name, number)
-  return json.dumps({'error': op_status})
+  op_error_status = add_contact(name, number)
+  return json.dumps({'error': op_error_status})
 
 
 if __name__ == "__main__":
-  app.run(debug=True, port=3001)
+  app.run(debug=True, port=3001, host='0.0.0.0')
+
--- src/save.py
+++
@@ -1,5 +0,0 @@
-def add_contact(name, number):
-  # Save a new contact and return either True
-  # or False indication the operation status
-  status = True # False by default, return the appropriate boolean
-  return status
